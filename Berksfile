135825eb6c3ae856aeb5647e2102b9ed173b2f04
kanbara <soel754@gmail.com>
2014-10-03 11:24:59 +0900

    first commit

---
+++ Berksfile
@@ -0,0 +1,8 @@
+source "https://supermarket.getchef.com"
+
+cookbook 'java', '~> 1.28.0'
+cookbook 'golang', '~> 1.4.0'
+cookbook 'rbenv', '~> 1.7.1'
+cookbook 'rbenv-gemset', '~> 0.1.1'
+cookbook 'yum', '~> 3.3.2'
+cookbook 'yum-epel', '~> 0.5.1'
---
+++ test/integration/default/serverspec/localhost/default_spec.rb
@@ -0,0 +1,26 @@
+require 'serverspec'
+include Serverspec::Helper::Exec
+include Serverspec::Helper::DetectOS
+
+Rspec.configure do |c|
+  c.before :all do
+    c.os = backend(Serverspec::Commands::Base).check_os
+  end
+  #c.path = "/sbin:/user/sbin"
+end
+
+describe command('/usr/bin/java -version') do
+  its(:stdout) { should match /1.7/ }
+end
+
+describe command('/usr/local/go/bin/go version') do
+  its(:stdout) { should match /go1.2.2/}
+end
+
+describe file('/opt/rbenv') do
+  it { should be_directory }
+end
+
+describe file('/etc/yum.repos.d/epel.repo') do
+  it { should be_file }
+end
