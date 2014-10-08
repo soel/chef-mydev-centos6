a47301e537eb487e5d7f6127747bc7410f57a524
kanbara <soel754@gmail.com>
2014-10-08 10:16:54 +0900

    fix serverspec error

--- test/integration/default/serverspec/localhost/default_spec.rb
+++ test/integration/default/serverspec/localhost/default_spec.rb
@@ -1,13 +1,15 @@
 require 'serverspec'
-include Serverspec::Helper::Exec
-include Serverspec::Helper::DetectOS
+#include Serverspec::Helper::Exec
+#include Serverspec::Helper::DetectOS
 
-Rspec.configure do |c|
-  c.before :all do
-    c.os = backend(Serverspec::Commands::Base).check_os
-  end
-  #c.path = "/sbin:/user/sbin"
-end
+#Rspec.configure do |c|
+#  c.before :all do
+#    c.os = backend(Serverspec::Commands::Base).check_os
+#  end
+#  c.path = "/sbin:/user/sbin"
+#end
+
+set :backend, :exec
 
 describe command('/usr/bin/java -version') do
   its(:stdout) { should match /1.7/ }
@@ -21,14 +23,6 @@ describe file('/opt/rbenv') do
   it { should be_directory }
 end
 
-describe file('/opt/rbenv/plugins/ruby_build')
-  it { should be_directory }
-end
-
-describe file('/opt/rbenv/plugins/rbenv-gemset')
-  it { should be_directory }
-end
-
-describe file('/etc/yum.repos.d/epel.repo') do
-  it { should be_file }
+describe yumrepo('epel') do
+  it { should exist }
 end
